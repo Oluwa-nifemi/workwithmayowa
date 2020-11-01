827856ad6a6583d1576bb942b3d0a48a08af4362
oluwa-nifemi <deyeminifemi@gmail.com>
2020-11-01 17:23:25 +0100

    Implemented homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,37 @@
-import React from "react"
+import React from 'react';
+import Layout from '../components/Layout';
+import classes from './index.module.css';
+import {Link} from 'gatsby';
+import potrait from '../assets/images/potrait.jpg';
 
 const Index = () => {
   return (
-    <div>
-      Home
-    </div>
+    <Layout
+      isPageHeight
+      mainClassName={classes.main}
+    >
+      <h1 className={classes.intro}>
+        Hello, I’m Mayowa Adeyemi, a {' '}
+        <Link
+          to='/works/writer'
+          className={classes.introLink}
+        >Writer
+        </Link> & {' '}
+        <Link
+          to='/works/designer'
+          className={classes.introLink}
+        >Designer.
+        </Link>
+      </h1>
+      <div className={classes.imageContainer}>
+        <div className={classes.imageBg}/>
+        <img
+          src={potrait}
+          alt="Mayowa"
+          className={classes.image}
+        />
+      </div>
+    </Layout>
   )
 }
 
