06edf58b6531fd8d0a4ebd5a261f46bedb532e1d
Nifemi <deyeminifemi@gmail.com>
2020-11-09 02:20:06 +0100

    Added config for page title

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -4,10 +4,17 @@ import Header from '../Header';
 import Footer from '../Footer';
 import {classNames} from '../../util/functions';
 
-const Layout = ({ children, isPageHeight = false, mainClassName, centerFooter = true }) => {
+const Layout = ({ children, isPageHeight = false, mainClassName, centerFooter = true, pageTitle }) => {
   return (
     <div className={classNames(classes.layout, isPageHeight && classes.layoutPageHeight)}>
       <Header/>
+      {
+        pageTitle && (
+          <h1 className={classes.pageTitle}>
+            {pageTitle}
+          </h1>
+        )
+      }
       <main className={classNames(mainClassName, classes.main)}>
         {children}
       </main>
