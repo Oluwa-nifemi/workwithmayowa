11303793c58abb88bdff3fb4c3d8d63a32a3adb4
Nifemi <deyeminifemi@gmail.com>
2020-11-12 01:05:50 +0100

    Radio page

---
+++ src/pages/work/writing/radio/index.js
@@ -0,0 +1,60 @@
+import React from "react"
+import Layout from "../../../../components/Layout"
+import classes from "./radio.module.css"
+import BackLink from "../../../../components/BackLink"
+
+const items = [
+  {
+    fileName: 'verve.mpeg',
+    name: 'Verve Life 3.0',
+    description: 'Verve Life is a fitness event organized by Verve. The theme for Verve Life 3.0 was “Go Hard or Go Home”'
+  },
+  {
+    fileName: 'gridcode.mpeg',
+    name: 'GridCode',
+    description: 'Gridcodes are easy unique codes that can be generated to digitize your unique address for easy location.'
+  },
+  {
+    fileName: 'fets.mp3',
+    name: 'Fetswallet IBEDC',
+    description: 'Children have creative, beautiful minds and these were created to celebrate their imagination.'
+  },
+  {
+    fileName: 'mtn.mp3',
+    name: 'MTN VTU Bonus',
+    description: 'MTN was introducing a bonus promo for airtime recharge done via Virtual Top-Up'
+  },
+]
+
+const Radio = () => {
+  return (
+    <Layout pageTitle='Radio/Audio'>
+      {
+        items.map(({name, description, fileName}) => (
+          <div className={classes.item}>
+            <audio
+              src={`/audio/${fileName}`}
+              controls
+              className={classes.itemAudio}
+            />
+            <div>
+              <h3 className={classes.itemName}>
+                {name}
+              </h3>
+              <p className={classes.itemDescription}>
+                {description}
+              </p>
+            </div>
+          </div>
+        ))
+      }
+      <div className={classes.backLinkWrapper}>
+        <BackLink to='/work/writing'>
+          Back To Writing
+        </BackLink>
+      </div>
+    </Layout>
+  )
+}
+
+export default Radio
