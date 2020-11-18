e98318c61e6fe3890a7219b2a2eeb7fbe5595bd4
Nifemi <deyeminifemi@gmail.com>
2020-11-19 00:35:30 +0100

    Function to convert array into arrays of max 3 length

--- src/util/functions.js
+++ src/util/functions.js
@@ -1,1 +1,14 @@
 export const classNames = (...klasses) => klasses.filter(className => Boolean(className)).join(" ")
+
+export const convertTo3By3Array = (array) => {
+  return array.reduce((currentImages, image) => {
+    let currentArray = currentImages[currentImages.length - 1];
+    if(currentArray.length === 3){
+      currentArray = []
+      currentImages.push(currentArray)
+    }
+    currentArray.push(image)
+
+    return currentImages
+  }, [[]])
+}
