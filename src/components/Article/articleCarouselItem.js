da004af767f3d24b1f81b4fe6bacddc511995022
Nifemi <deyeminifemi@gmail.com>
2020-11-18 22:26:04 +0100

    Carousel item

---
+++ src/components/Article/articleCarouselItem.js
@@ -0,0 +1,24 @@
+import React from "react"
+import classes from "./article.module.css"
+import Img from "gatsby-image"
+
+const ArticleCarouselItem = ({fluid, name, maxWidth = '100%'}) => {
+  return (
+    <div
+      className={classes.articleCarouselItem}
+      style={{
+        maxWidth: maxWidth
+      }}
+    >
+      <Img
+        fluid={fluid}
+        alt={name}
+      />
+      <p className={classes.articleCarouselItemText}>
+        {name}
+      </p>
+    </div>
+  )
+}
+
+export default ArticleCarouselItem
