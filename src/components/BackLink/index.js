178f38f46951ec6569fda30f7b40b4e493925170
Nifemi <deyeminifemi@gmail.com>
2020-11-18 23:02:34 +0100

    Added classname prop

--- src/components/BackLink/index.js
+++ src/components/BackLink/index.js
@@ -2,10 +2,11 @@ import React from "react"
 import { Link } from "gatsby"
 import classes from "./backLink.module.css"
 import arrow from "../../assets/images/arrow.svg"
+import { classNames } from "../../util/functions"
 
-const BackLink = ({ to, children }) => {
+const BackLink = ({ to, children, className }) => {
   return (
-    <Link to={to} className={classes.backLink}>
+    <Link to={to} className={classNames(classes.backLink, className)}>
       <img src={arrow} alt="Arrow" className={classes.backLinkImg}/>
       <span className={classes.backLinkText}>
         {children}
