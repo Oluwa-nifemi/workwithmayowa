75b3dde1a0f37475b120c35ae6c96a7169b0960e
Nifemi <deyeminifemi@gmail.com>
2020-11-18 22:08:32 +0100

    Added config for small text

--- src/components/Article/articleText.js
+++ src/components/Article/articleText.js
@@ -1,12 +1,15 @@
 import React from "react"
 import classes from "./article.module.css"
+import { classNames } from "../../util/functions"
 
-const ArticleText = ({children}) => {
+const ArticleText = ({children, smallMargin}) => {
   return (
-    <p className={classes.articleText}>
+    <p className={classNames(classes.articleText, smallMargin && classes.articleTextSmallMargin)}>
       {children}
     </p>
   )
 }
+
+export const ArticleTextSmall = (props) => <ArticleText {...props} smallMargin/>
 
 export default ArticleText
