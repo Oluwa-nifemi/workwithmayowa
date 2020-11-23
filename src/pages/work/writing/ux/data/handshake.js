1327fb9f57df2c2c5e212e3cd14655440eb0d913
Nifemi <deyeminifemi@gmail.com>
2020-11-23 01:09:49 +0100

    Setup mapping of names to allow properly sorted carousel items

---
+++ src/pages/work/writing/ux/data/handshake.js
@@ -0,0 +1,13 @@
+export const setupMapping = {
+  1: 'Previous Client Set-Up Screens',
+  2: 'Redesigned Client Set-Up Screens',
+  3: 'Previous Freelancer Set-Up Screens',
+  4: 'Redesigned Freelancer Set-Up Screens',
+  5: 'Success Modal Screens'
+}
+
+export const freelancerMapping = {
+  1: 'Previous Freelancer Ongoing Use Screens',
+  2: 'Redesigned Freelancer Ongoing Use Screens',
+  3: 'Redesigned Freelancer Ongoing Use Screens'
+}
