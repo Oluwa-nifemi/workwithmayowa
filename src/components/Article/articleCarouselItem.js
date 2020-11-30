2498b660d70256da00f679eb0df71a71219eba4e
Nifemi <deyeminifemi@gmail.com>
2020-11-30 19:19:27 +0100

    Conditionally render name

--- src/components/Article/articleCarouselItem.js
+++ src/components/Article/articleCarouselItem.js
@@ -2,7 +2,7 @@ import React from "react"
 import classes from "./article.module.css"
 import Img from "gatsby-image"
 
-const ArticleCarouselItem = ({fluid, name, maxWidth = '100%'}) => {
+const ArticleCarouselItem = ({ fluid, name, maxWidth = "100%" }) => {
   return (
     <div
       className={classes.articleCarouselItem}
@@ -14,9 +14,13 @@ const ArticleCarouselItem = ({fluid, name, maxWidth = '100%'}) => {
         fluid={fluid}
         alt={name}
       />
-      <p className={classes.articleCarouselItemText}>
-        {name}
-      </p>
+      {
+        name && (
+          <p className={classes.articleCarouselItemText}>
+            {name}
+          </p>
+        )
+      }
     </div>
   )
 }
