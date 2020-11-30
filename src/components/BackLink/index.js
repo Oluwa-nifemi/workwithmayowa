d2f8b4b87007cecdd0213cf1360021ad053ad047
Nifemi <deyeminifemi@gmail.com>
2020-11-30 18:43:50 +0100

    Spread extra props

--- src/components/BackLink/index.js
+++ src/components/BackLink/index.js
@@ -4,9 +4,9 @@ import classes from "./backLink.module.css"
 import arrow from "../../assets/images/arrow.svg"
 import { classNames } from "../../util/functions"
 
-const BackLink = ({ to, children, className }) => {
+const BackLink = ({ to, children, className, ...props }) => {
   return (
-    <Link to={to} className={classNames(classes.backLink, className)}>
+    <Link to={to} className={classNames(classes.backLink, className)} {...props}>
       <img src={arrow} alt="Arrow" className={classes.backLinkImg}/>
       <span className={classes.backLinkText}>
         {children}
