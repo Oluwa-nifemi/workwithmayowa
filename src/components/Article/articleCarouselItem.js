6ca1c19996e82c073c75e1295c893a06e3d57c4e
Nifemi <deyeminifemi@gmail.com>
2020-12-23 01:05:32 +0100

    Used tap zoom in article carousel item

--- src/components/Article/articleCarouselItem.js
+++ src/components/Article/articleCarouselItem.js
@@ -1,27 +1,37 @@
 import React from "react"
 import classes from "./article.module.css"
 import Img from "gatsby-image"
+import TapToZoom from "../TapToZoom"
 
 const ArticleCarouselItem = ({ fluid, name, maxWidth = "100%" }) => {
   return (
-    <div
-      className={classes.articleCarouselItem}
-      style={{
-        maxWidth: maxWidth
-      }}
+    <TapToZoom
+      modalContent={(
+        <Img
+          fluid={fluid}
+          alt={name}
+        />
+      )}
     >
-      <Img
-        fluid={fluid}
-        alt={name}
-      />
-      {
-        name && (
-          <p className={classes.articleCarouselItemText}>
-            {name}
-          </p>
-        )
-      }
-    </div>
+      <div
+        className={classes.articleCarouselItem}
+        style={{
+          maxWidth: maxWidth
+        }}
+      >
+        <Img
+          fluid={fluid}
+          alt={name}
+        />
+        {
+          name && (
+            <p className={classes.articleCarouselItemText}>
+              {name}
+            </p>
+          )
+        }
+      </div>
+    </TapToZoom>
   )
 }
 
